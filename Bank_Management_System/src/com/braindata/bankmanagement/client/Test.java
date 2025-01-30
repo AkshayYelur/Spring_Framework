ceaa30fdf6ca416199cf940b2f0072094c3accb5
Akshay-knows <72262127+Akshay-knows@users.noreply.github.com>
2025-01-30 16:17:04 +0530

    new ADDED

--- Bank_Management_System/src/com/braindata/bankmanagement/client/Test.java
+++ Bank_Management_System/src/com/braindata/bankmanagement/client/Test.java
@@ -10,6 +10,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Bank Management System");
 		//////
+		/////////
 	Scanner sc=new Scanner(System.in);
 		
 		Rbi r=new Sbi();
